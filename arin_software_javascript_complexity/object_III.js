aaff19170bde6e50dfbb676bd6705877244a0bd7
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-05-28 20:19:54 +0300

    object III

---
+++ arin_software_javascript_complexity/object_III.js
@@ -0,0 +1,63 @@
+// New Object Syntax
+
+// 1- Shorthand Properties
+
+/*let name = 'arin', age = 5;
+
+const myObj = {
+    name,
+    age
+}
+
+// 2- Computed Property Names
+
+let prop1 = 'name';
+let myObj = {
+}
+myObj[prop1] = 'arin'
+
+console.log(myObj);
+
+let prop1 = 'name';
+
+let myObj = {
+    [prop1]: 'arin'
+}
+
+console.log(myObj)
+
+//3- Shorted Method Syntax
+
+const person ={
+    name: "john",
+    surname: "Doe",
+    age: 40,
+    fullName(){
+        return this.name + " " + this.surname
+    }
+}
+console.log(person.fullName());*/
+
+// 4- Object Destructuring
+
+/*const person ={
+name: "john",
+surname: "Doe",
+age: 40,
+fullName(){
+    return this.name + " " + this.surname
+}
+}*/
+/*let myVar1 = person.name;
+console.log(myVar1);
+
+let myVar2 = person.age;
+console.log(myVar2);*/
+
+
+/*let {name:myVar1, age:myVar2}= person;
+console.log(myVar1);
+console.log(myVar2);*/
+
+
+ 
